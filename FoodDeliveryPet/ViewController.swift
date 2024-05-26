2f8b3b1915da70f1678dd17114509aa3ad64b6c1
maksimir91 <maksimirrus@gmail.com>
2024-05-26 17:11:45 +0300

    added new fonts

--- FoodDeliveryPet/ViewController.swift
+++ FoodDeliveryPet/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = "Hello World"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20)
+        ])
+        label.font = .systemFont(ofSize: 40, weight: UIFont.Weight(rawValue: 20))
+        label.textColor = .white
     }
 
 
