530c4df4038239fa535ae1d2c269c524e02b4b1b
maksimir91 <maksimirrus@gmail.com>
2024-05-26 16:13:41 +0300

    Initial Commit

---
+++ FoodDeliveryPet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryPet
+//
+//  Created by Stanislav Shut on 26.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
