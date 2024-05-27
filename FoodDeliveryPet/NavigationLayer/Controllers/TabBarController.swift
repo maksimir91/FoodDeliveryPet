bd056cdd9882e2e1da3776f06505a10e4a87b3df
maksimir91 <maksimirrus@gmail.com>
2024-05-27 18:09:54 +0300

    added other coordinators and tab bar controller

---
+++ FoodDeliveryPet/NavigationLayer/Controllers/TabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  TabBarController.swift
+//  FoodDeliveryPet
+//
+//  Created by Stanislav Shut on 27.05.2024.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    init(tabBarControllers: [UIViewController]) {
+        super.init(nibName: nil, bundle: nil)
+        for tab in tabBarControllers {
+            self.addChild(tab)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.backgroundColor = AppColors.background
+        tabBar.tintColor = AppColors.accentOrange
+    }
+}
---
+++ FoodDeliveryPet/NavigationLayer/Coordinators/ListCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  ListCoordinator.swift
+//  FoodDeliveryPet
+//
+//  Created by Stanislav Shut on 27.05.2024.
+//
+
+import UIKit
+
+class ListCoordinator: Coordinator {
+    override func start() {
+        let vc = ViewController()
+        vc.view.backgroundColor = .green
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    override func finish() {
+        print("AppCoordinator finish")
+    }
+}
