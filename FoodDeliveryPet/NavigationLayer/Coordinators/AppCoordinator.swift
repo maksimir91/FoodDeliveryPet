e8d5ab95aa7442931106ad93ac73f608d7e47f91
maksimir91 <maksimirrus@gmail.com>
2024-06-28 17:05:18 +0300

    added login coordinator, fixed strong references

--- FoodDeliveryPet/NavigationLayer/Coordinators/AppCoordinator.swift
+++ FoodDeliveryPet/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -29,7 +29,8 @@ class AppCoordinator: Coordinator {
 private extension AppCoordinator {
     func showOnboardingFlow() {
         guard let navigationController = navigationController else { return }
-        factory.makeOnboardingFlow(coordinator: self, finishDelegate: self, navigationController: navigationController)
+       let onboardingCoordinator = factory.makeOnboardingFlow(coordinator: self, finishDelegate: self, navigationController: navigationController)
+        onboardingCoordinator.start()
     }
     
     func showMainFlow() {
@@ -39,28 +40,12 @@ private extension AppCoordinator {
     }
     func showAuthFlow() {
         guard let navigationController = navigationController else { return }
-        let vc = factory.makeAuthScene(coordinator: self)
-        navigationController.pushViewController(vc, animated: true)
+        let loginCoordinator = factory.makeLoginFlow(coordinator: self, finishDelegate: self, navigationController: navigationController)
+        loginCoordinator.start()
     }
 }
 
-// MARK: - Methods
-extension AppCoordinator {
-    func showSignInScene() {
-        guard let navigationController = navigationController else { return }
-        let vc = factory.makeSignInScene(coordinator: self)
-        navigationController.pushViewController(vc, animated: true)
-    }
-    func showSignUpScene() {
-        guard let navigationController = navigationController else { return }
-        let vc = factory.makeSignUpScene(coordinator: self)
-        navigationController.pushViewController(vc, animated: true)
-    }
-    func showMainScene() {
-        showMainFlow()
-    }
-}
-
+// MARK: - FinishDelegate
 extension AppCoordinator: CoordinatorFinishDelegate {
     func coordinatorDidFinish(childCoordinator: CoordinatorProtocol) {
         removeChildCoordinator(childCoordinator)
@@ -69,6 +54,9 @@ extension AppCoordinator: CoordinatorFinishDelegate {
         case .onboarding:
             navigationController?.viewControllers.removeAll()
             showAuthFlow()
+        case .login:
+            navigationController?.viewControllers.removeAll()
+            showMainFlow()
         case .app:
             return
         default:
